e5c426db84abf58dfd393fd565c07f4ff4527fd1
greenwoodmultimedia <greenwoodmultimedia@users.noreply.github.com>
2020-09-30 16:53:50 -0400

    Ajout d'animation du SplashScreen et des autres scènes du jeu
    Ajout des liens de chargement des autres scènes et de leur noeud respectif Main. Ajout d'une animation gérer par des Timers du SplashScreen.

--- Devoir 4/JeuSpaceShot/scripts/Main.gd
+++ Devoir 4/JeuSpaceShot/scripts/Main.gd
@@ -1,29 +1,76 @@
+# Jeu fait par Christopher Boisvert © 2020 - Tous droits reservés.
 extends Node2D
 
-# Jeu fait par Christopher Boisvert © 2020 - Tous droits reservés.
+#Variables
+var timer_logo;
+var timer_bouton_start;
+var timer_bouton_options;
+var timer_bouton_quitter;
+onready var logo = $logo;
+onready var boutonStart = $boutonStart;
+onready var boutonOptions = $boutonOptions;
+onready var boutonQuitter = $boutonQuitter;
 
-
-# Called when the node enters the scene tree for the first time.
+# Fonction _ready
+# On passe sans rien faire !
 func _ready():
-	pass # Replace with function body.
+	
+	# On instancie les timers
+	timer_logo = Timer.new();
+	timer_bouton_start = Timer.new();
+	timer_bouton_options = Timer.new();
+	timer_bouton_quitter = Timer.new();
+	
+	timer_logo.set_wait_time(1);
+	timer_bouton_start.set_wait_time(2);
+	timer_bouton_options.set_wait_time(3);
+	timer_bouton_quitter.set_wait_time(4);
+	
+	timer_logo.connect("timeout",self,"_on_timer_timeout_logo");
+	timer_bouton_start.connect("timeout",self,"_on_timer_timeout_bouton_start");
+	timer_bouton_options.connect("timeout",self,"_on_timer_timeout_bouton_options");
+	timer_bouton_quitter.connect("timeout",self,"_on_timer_timeout_bouton_quitter");
+	
+	add_child(timer_logo);
+	add_child(timer_bouton_start);
+	add_child(timer_bouton_options);
+	add_child(timer_bouton_quitter);
+	
+	timer_logo.start();
+	timer_bouton_start.start();
+	timer_bouton_options.start();
+	timer_bouton_quitter.start();
 
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
-
+# Fonction _on_aread2DStart_input_event
+# Survient quand un événement est généré par area2DStart
+# Si l'événement est un clic, on change de scène
 func _on_aread2DStart_input_event(viewport, event, shape_idx):
 	if  event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
-		print("TODO: Start game !");
+		get_tree().change_scene("res://scenes/Game.tscn");
 
-
+# Fonction _on_aread2DOptions_input_event
+# Survient quand un événement est généré par aread2DOptions
+# Si l'événement est un clic, on change de scène
 func _on_aread2DOptions_input_event(viewport, event, shape_idx):
 	if  event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
-		print("TODO: Options of game !");
+		get_tree().change_scene("res://scenes/Options.tscn");
 
-
+# Fonction _on_area2DQuitter_input_event
+# Survient quand un événement est généré par area2DQuitter
+# Si l'événement est un clic, on ferme le programme
 func _on_area2DQuitter_input_event(viewport, event, shape_idx):
 	if  event is InputEventMouseButton and event.pressed and event.button_index == BUTTON_LEFT:
 		print("Quitting game !");
 		get_tree().quit();
+		
+func _on_timer_timeout_logo():
+	logo.visible = true;
+	
+func _on_timer_timeout_bouton_start():
+	boutonStart.visible = true;
+	
+func _on_timer_timeout_bouton_options():
+	boutonOptions.visible = true;
+	
+func _on_timer_timeout_bouton_quitter():
+	boutonQuitter.visible = true;
